8d76fe077774dfe1a6ee7cad1cfbec3026c7837e
lorisgoglia <loris.goglia@gmail.com>
2024-01-08 19:13:25 +0100

    Update inputForm.js

--- src/components/inputForm.js
+++ src/components/inputForm.js
@@ -1,6 +1,6 @@
 // src/components/InputForm.js
 import React, { useState } from 'react';
-import { Flex, Input, Button, Box, Text, Heading } from '@chakra-ui/react';
+import { Flex, Input, Button, Box, Text, Heading, Alert, AlertIcon } from '@chakra-ui/react';
 
 const InputForm = () => {
   const [email, setEmail] = useState('');
@@ -8,7 +8,7 @@ const InputForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setEmail(e.currentTarget.email.value);
+    setEmail(e.currentTarget.email);
     setSubscribed(true);
   };
 
@@ -17,9 +17,39 @@ const InputForm = () => {
     <Flex direction="column" align="center" justify="center" bg="" p="8" color="black">
       <Box mt="4" w="full" textAlign="center">
         {subscribed ? (
-          <Text fontSize="lg" mb="4">
-            Thank you for subscribing!
+
+          <>
+                        
+          <Heading fontSize="3xl" fontWeight="bold" mb="4">
+            Subscribe to our newsletter
+          </Heading>
+          <Text mb="6">
+            Stay up-to-date with the latest news, events, and exclusive offers!
           </Text>
+
+          <form onSubmit={handleSubmit}>
+          <Flex>
+            
+            <Input type="email" title="Email example: ritmos@gmail.com" placeholder="Enter your email" aria-label="Enter your email to subscribe" variant="filled" mr="2" isRequired name="Enter your email" autocomplete="email" aria-required="true"/>
+            <Button 
+              type="submit" 
+              color="#ffffff"
+              bgColor="#2b74b9"
+              aria-label='Click the button to subscribe'
+              _hover={{ bgColor: '#125580' }}
+              isDisabled="true"
+              
+              >
+              Subscribe
+            </Button>
+          </Flex>
+          </form>
+
+          <Alert status='success' mt={5} role='status'>
+          <AlertIcon />
+          Thank you for subscribing!
+        </Alert>
+        </>
         ) : (
           <>
               
